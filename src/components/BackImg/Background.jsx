633ab19f2d757f6803f7852ebe25105a84c4778e
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-29 21:33:05 +0900

    スクロールアニメーション実装した

--- src/components/BackImg/Background.jsx
+++ src/components/BackImg/Background.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Background.scss";
+import { motion, useScroll, useTransform } from "framer-motion"
 
 const Background = ({ type }) => {
+
+    const ref = useRef();
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end start"]
+    });
+
+    const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+    const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
     return (
-        <div className='background' style={{
-            background: type === "Services"
-                ? "linear-gradient(180deg, rgb(46, 44, 44), rgb(73, 70, 70)"
-                : "linear-gradient(180deg, rgb(46, 44, 44), rgb(101, 90, 90)"
-        }}>
-            <h1>{type === "Services" ? "What we do ?" : "What we did ?"}</h1>
-            <div className='mountains'></div>
-            <div className='planets'></div>
-            <div className='stars'></div>
+        <div className='background'
+            ref={ref}
+            style={{
+                background: type === "Services"
+                    ? "linear-gradient(180deg, rgb(46, 44, 44), rgb(73, 70, 70)"
+                    : "linear-gradient(180deg, rgb(46, 44, 44), rgb(101, 90, 90)"
+            }}>
+            <motion.h1 style={{ y: yText }}>{type === "Services" ? "What we do ?" : "What we did ?"}</motion.h1>
+            <motion.div className='mountains'></motion.div>
+            <motion.div className='planets'
+                style={{ y: yBg, backgroundImage: `url(${type === "Services" ? "/planets.png" : "/sun.png"})` }}
+            ></motion.div>
+            <motion.div style={{ x: yBg }} className='stars'></motion.div>
         </div>
     )
 }
