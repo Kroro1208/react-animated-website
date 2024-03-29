3c90da8df4ab14a92d4676ed2055e81f16fb2e8c
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-29 18:52:41 +0900

    heroコンポーネントのアニメーション実装した

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,24 +1,60 @@
 import React from 'react'
 import { CgScrollV } from "react-icons/cg";
 import "./hero.scss"
+import { motion } from "framer-motion"
 
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    },
+    scrollButton: {
+        opacity: 1,
+        y: 20,
+        transition: {
+            duration: 2,
+            repeat: Infinity
+        }
+    },
+
+
+}
 
 const Hero = () => {
     return (
         <div className='hero'>
-            <div className="text-container">
-                <h1>NAOYA YAMAGATA</h1>
-                <h2>Web Developer</h2>
-                <div>
-                    <button>開発実績</button>
-                    <button>お問い合わせ</button>
-                </div>
-                <CgScrollV />
+            <div className='wrapper'>
+                <motion.div className="text-container"
+                    variants={textVariants}
+                    initial="initial"
+                    animate="animate"
+                >
+                    <motion.h1 variants={textVariants}>NAOYA YAMAGATA</motion.h1>
+                    <motion.h2 variants={textVariants}>Web Developer</motion.h2>
+                    <motion.div className='buttons' variants={textVariants}>
+                        <motion.button variants={textVariants}>開発実績</motion.button>
+                        <motion.button variants={textVariants}>お問い合わせ</motion.button>
+                    </motion.div>
+                    <motion.div variants={textVariants} animate="scrollButton">
+                        <CgScrollV />
+                    </motion.div>
+                </motion.div>
+            </div>
+            <div className='sliding-text-container'>
+                Frontend Backend Shopify
             </div>
             <div className='image-container'>
                 <img src="/sns.png" alt="" />
             </div>
-        </div>
+        </div >
     )
 }
 
