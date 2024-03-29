aa3bc5ae3c98f6058fb8ee3da7ce75ded21ce7d2
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-29 15:20:35 +0900

    環境設定とNavbarレイアウト実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section><Navbar /></section>
+    <section>BackImage</section>
+    <section>Service</section>
+    <section>BackImage2</section>
+    <section>Portfolio</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>contact</section>
+  </div>;
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./navbar.scss"
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { TfiYoutube } from "react-icons/tfi";
+import { PiTwitterLogoBold } from "react-icons/pi";
+
+
+
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <span>Wed Developer</span>
+                <div className="social">
+                    <a href="#"><FaFacebookSquare /></a>
+                    <a href="#"><FaInstagram /></a>
+                    <a href="#"><TfiYoutube /></a>
+                    <a href="#"><PiTwitterLogoBold /></a>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default Navbar
